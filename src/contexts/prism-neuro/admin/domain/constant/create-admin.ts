3c77d27d04cb4f0712a1f8d83508cc5f402e8e58
prasidha <webtecsolutions@Webtecs-MacBook-Pro.local>
2024-07-03 13:03:14 +0545

    admin ===> set up file for admin seeder

---
+++ src/contexts/prism-neuro/admin/domain/constant/create-admin.ts
@@ -0,0 +1,8 @@
+import { ICreateAdmin } from '../interface/create-admin';
+
+export const adminUser: ICreateAdmin = {
+  email: 'admin@gmail.com',
+  password: 'admin@@1',
+  name: 'Prism Neuro',
+  address: 'Kathmandu,Nepal'
+};
---
+++ src/contexts/prism-neuro/admin/domain/interface/create-admin.ts
@@ -0,0 +1,6 @@
+export interface ICreateAdmin {
+  email: string;
+  password: string;
+  name: string;
+  address: string;
+}
