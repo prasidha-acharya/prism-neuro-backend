7cb10d6014f0813a68ce4f24317311095f188a36
Webtec Solutions <webtecsolutions@Webtecs-MacBook-Pro.local>
2024-07-03 08:40:06 +0545

    Keep calm and test husky

---
+++ lint-staged.config.js
@@ -0,0 +1,19 @@
+// lint-staged.config.js
+module.exports = {
+    // Type check TypeScript files
+    '**/*.(ts|tsx)': () => 'npx tsc --noEmit',
+  
+    // Run ESLint on changes to JavaScript/TypeScript files
+    '**/*.(ts|js)?(x)': () => `eslint . --ext ts --ext tsx `,
+  
+    // Lint then format TypeScript and JavaScript files
+    '**/*.(ts|tsx|js)': filenames => [
+      `npx eslint --fix ${filenames.join(' ')}`,
+      `npx  prettier --write ${filenames.join(' ')}`
+      // `npx test`
+    ],
+  
+    // Format MarkDown and JSON
+    '**/*.(md|json)': filenames => `npx  prettier --write ${filenames.join(' ')}`
+  };
+  
