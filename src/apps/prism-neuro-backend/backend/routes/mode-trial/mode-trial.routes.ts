7f59c57a40ab257a921a4fa004f3ede95cc91d3b
prasidha <webtecsolutions@Webtecs-MacBook-Pro.local>
2024-07-19 10:49:33 +0545

    mode/trial-start >>>>> removed form route

--- src/apps/prism-neuro-backend/backend/routes/mode-trial/mode-trial.routes.ts
+++ src/apps/prism-neuro-backend/backend/routes/mode-trial/mode-trial.routes.ts
@@ -14,43 +14,7 @@ export const modeTrialRoutesHandler = (
   adminAuthorizer: IAuthorizer<Request, Response, NextFunction>,
   router: Router
 ): Router => {
-  const { startModeTrialController, endModeTrialController, getModeTrialBySessionController } = controller;
-
-  router.post(
-    '/mode/trial-start/:modeId',
-    physioAuthorizer.authorize,
-    startModeTrialController.validate,
-    startModeTrialController.invoke.bind(startModeTrialController)
-    /* 
-     #swagger.security = [{
-            "bearerAuth": []
-    }] 
-    #swagger.tags = ['Mode Trial']
-    #swagger.summary="Only Physio therapist can start trial"
-    #swagger.description="Starts trial"
-    #swagger.parameters['modeId'] = {
-    in:"path",
-    type:"string",
-    required:"true"
-    }
-      #swagger.requestBody = {
-      required: true,
-      content: {
-        "application/json": {
-          schema: {
-            type: "object",
-            required: ["trialId", "startTime","sessionId"],
-            properties: {
-              trialId: { type: "number" },
-              startTime: { type: "date"},
-              sessionId: {type: "string"}
-            }
-          }
-        }
-      }
-    }
-    */
-  );
+  const { endModeTrialController, getModeTrialBySessionController } = controller;
 
   router.post(
     '/mode/trial-end/:modeId',
@@ -77,10 +41,10 @@ export const modeTrialRoutesHandler = (
             type: "object",
             required: ['startTime","endTime" ,"trialId","results" ,"rawData","sessionId"],
             properties: {
-              startTime:{type:"string"}
+              startTime:{type:"string"},
               endTime: { type:"string"},
               sessionId: { type:"string"},
-              trialId:{type:"number"}
+              trialId:{type:"number"},
               results: { type: "object" },
               rawData: { type:"object"},
             }
