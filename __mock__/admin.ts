ba4b83d001173016082172a6ad5a1103471fe00b
prasidha <Prasidha.acharya@cubit.com.np>
2024-07-30 10:17:59 +0545

    created mock folder to store static data

---
+++ __mock__/admin.ts
@@ -0,0 +1,10 @@
+import { ICreateAdmin } from 'src/contexts/prism-neuro/users/domain/interface/user-request.interface';
+
+export const adminUser: ICreateAdmin = {
+  email: 'admin@gmail.com',
+  password: 'admin@@1',
+  // userName: 'Prism Neuro',
+  address: [{ address: 'Kathmandu,Nepal' }]
+};
+
+export const expiresAfter15Days = new Date(new Date().getTime() + 15 * 60 * 1000);
