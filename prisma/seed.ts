ef50fcb5eaa8ed18e9f2fb3fa14e76e6538b56f6
prasidha <Prasidha.acharya@cubit.com.np>
2024-07-30 10:57:38 +0545

    wip >>>> seed

---
+++ prisma/seed.ts
@@ -0,0 +1,35 @@
+import { PrismaClient, USER_ROLES } from '@prisma/client';
+import { adminUser } from '__mock__/admin';
+
+const prisma = new PrismaClient();
+
+async function main(): Promise<void> {
+  //admin
+
+  const admin = await prisma.user.create({
+    data: {
+      email: adminUser.email,
+      role: USER_ROLES.ADMIN,
+      password: adminUser.password,
+      userAddress: adminUser.address && {
+        createMany: {
+          data: adminUser.address
+        }
+      }
+    }
+  });
+
+  //mode
+
+  console.log(admin);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async e => {
+    console.log(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
