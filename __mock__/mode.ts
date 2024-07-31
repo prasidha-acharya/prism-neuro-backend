951a212a2c494fb5989417476ba87c4ff33dc5bf
prasidha <Prasidha.acharya@cubit.com.np>
2024-07-31 14:05:53 +0545

    update mode

--- __mock__/mode.ts
+++ __mock__/mode.ts
@@ -2,26 +2,32 @@ import { MODE_TYPE } from '@prisma/client';
 
 export const modeTypes = [
   {
-    type: MODE_TYPE.BALANCE_MODE,
-    instructions: [
-      'Place the balance board on a stable surface and stand comfortably.',
-      'Shift your weight gently from side to side on the board.',
-      'Evaluate your balance and movement as you shift your body.'
-    ],
     name: 'Balance Mode-with Feedback',
+    images: [],
     trialCount: 3,
-    trialDuration: 30
+    trialDuration: 30,
+    type: MODE_TYPE.BALANCE_MODE,
+    modeDetail: {
+      instructions: [
+        'Place the balance board on a stable surface and stand comfortably.',
+        'Shift your weight gently from side to side on the board.',
+        'Evaluate your balance and movement as you shift your body.'
+      ]
+    }
   },
   {
     type: MODE_TYPE.TARGET_MODE,
-    instructions: [
-      'Position yourself stably on the balance board.',
-      'Move towards each target displayed on the screen.',
-      'Complete the task by accurately targeting as many points as possible within 30 seconds.'
-    ],
     name: 'Target Mode',
     trialCount: 3,
-    trialDuration: 30
+    trialDuration: 30,
+    images: [],
+    modeDetail: {
+      instructions: [
+        'Position yourself stably on the balance board.',
+        'Move towards each target displayed on the screen.',
+        'Complete the task by accurately targeting as many points as possible within 30 seconds.'
+      ]
+    }
   },
   {
     type: MODE_TYPE.LEFT_RIGHT_MODE,
@@ -31,7 +37,16 @@ export const modeTypes = [
       'Concentrate on responding accurately and promptly to the directional prompts given by the displayed images.'
     ],
     name: 'Left and Right Mode',
-    trialCount: 3
+    trialCount: 3,
+    images: [],
+    modeDetail: {
+      imageUrls: [],
+      instructions: [
+        'Position yourself stably on the balance board.',
+        'Move towards each target displayed on the screen.',
+        'Complete the task by accurately targeting as many points as possible within 30 seconds.'
+      ]
+    }
   },
   {
     type: MODE_TYPE.VISUAL_BALANCE_MODE,
@@ -41,6 +56,15 @@ export const modeTypes = [
       'Evaluate your balance and movement as you shift your body.'
     ],
     name: 'Balance Mode with visuals-no Feedback',
-    trialCount: 3
+    trialCount: 3,
+    images: [],
+    modeDetail: {
+      videoUrls: [],
+      instructions: [
+        'Position yourself stably on the balance board.',
+        'Move towards each target displayed on the screen.',
+        'Complete the task by accurately targeting as many points as possible within 30 seconds.'
+      ]
+    }
   }
 ];
